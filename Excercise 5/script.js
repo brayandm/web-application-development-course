eb9ae84947217ee5093a64bf076ff750e906a7ec
Brayan Durán Medina <brayanduranmedina@gmail.com>
2024-06-14 19:24:47 +0200

    updating script

--- Excercise 5/script.js
+++ Excercise 5/script.js
@@ -1,21 +1,13 @@
-window.onload = function () {
-    const addButton = document.getElementById("add");
-    const subButton = document.getElementById("subtract");
+const addButton = document.getElementById("add");
+const subButton = document.getElementById("subtract");
+const input1 = document.getElementById("num1");
+const input2 = document.getElementById("num2");
+const result = document.getElementById("result");
 
-    addButton.addEventListener("click", function () {
-        console.log("Hello World2");
-        const input1 = document.getElementById("num1");
-        const input2 = document.getElementById("num2");
+addButton.addEventListener("click", function () {
+    result.value = parseInt(input1.value) + parseInt(input2.value);
+});
 
-        const result = document.getElementById("result");
-        result.value = parseInt(input1.value) + parseInt(input2.value);
-    });
-
-    subButton.addEventListener("click", function () {
-        const input1 = document.getElementById("num1");
-        const input2 = document.getElementById("num2");
-
-        const result = document.getElementById("result");
-        result.value = parseInt(input1.value) - parseInt(input2.value);
-    });
-};
+subButton.addEventListener("click", function () {
+    result.value = parseInt(input1.value) - parseInt(input2.value);
+});
