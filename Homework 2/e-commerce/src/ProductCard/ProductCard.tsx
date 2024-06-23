6715e4820fa59241fe372bf9cd980a32600377ff
Brayan Durán Medina <brayanduranmedina@gmail.com>
2024-06-23 16:20:14 +0200

    feat: Improve rendering performance with lazy loading for product images

--- Homework 2/e-commerce/src/ProductCard/ProductCard.tsx
+++ Homework 2/e-commerce/src/ProductCard/ProductCard.tsx
@@ -1,3 +1,13 @@
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import CardMedia from "@mui/material/CardMedia";
+import CardContent from "@mui/material/CardContent";
+import CardActions from "@mui/material/CardActions";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import ShareIcon from "@mui/icons-material/Share";
+
 export interface Product {
     id: number;
     title: string;
@@ -13,5 +23,32 @@ interface ProductCardProps {
 }
 
 export default function ProductCard({ product }: ProductCardProps) {
-    return <></>;
+    return (
+        <Card sx={{ maxWidth: 345 }}>
+            <CardHeader
+                title={product.title}
+                subheader={product.category}
+                sx={{}}
+            />
+            <CardMedia
+                component="img"
+                height="194"
+                image={product.image}
+                alt={product.title}
+            />
+            <CardContent>
+                <Typography variant="body2" color="text.secondary">
+                    {product.description}
+                </Typography>
+            </CardContent>
+            <CardActions disableSpacing>
+                <IconButton aria-label="add to favorites">
+                    <FavoriteIcon />
+                </IconButton>
+                <IconButton aria-label="share">
+                    <ShareIcon />
+                </IconButton>
+            </CardActions>
+        </Card>
+    );
 }
--- Homework 2/e-commerce/src/Products/Products.tsx
+++ Homework 2/e-commerce/src/Products/Products.tsx
@@ -1,10 +1,23 @@
-import { Product } from "../ProductCard/ProductCard";
+import ProductCard, { Product } from "../ProductCard/ProductCard";
 
 interface ProductsProps {
     products: Product[];
 }
 
 export default function Products({ products }: ProductsProps) {
-    console.log(products);
-    return <></>;
+    return (
+        <div
+            style={{
+                display: "flex",
+                flexWrap: "wrap",
+                marginTop: "20px",
+                gap: "20px",
+                justifyContent: "center",
+            }}
+        >
+            {products.map((product) => (
+                <ProductCard key={product.id} product={product} />
+            ))}
+        </div>
+    );
 }
