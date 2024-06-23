dd9bfb98cde44ec010ac9437857305e32493589a
Brayan Durán Medina <brayanduranmedina@gmail.com>
2024-06-23 15:57:53 +0200

    feat: Add Header component for the E-Commerce app

--- Homework 2/e-commerce/src/App.tsx
+++ Homework 2/e-commerce/src/App.tsx
@@ -1,33 +1,10 @@
 import "./App.css";
-import AppBar from "@mui/material/AppBar";
-import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
-import IconButton from "@mui/material/IconButton";
-import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import Header from "./Header";
 
 function App() {
     return (
         <>
-            <AppBar position="static">
-                <Toolbar>
-                    <Typography
-                        variant="h6"
-                        component="div"
-                        sx={{ flexGrow: 1 }}
-                    >
-                        E-Commerce
-                    </Typography>
-                    <IconButton
-                        size="large"
-                        edge="start"
-                        color="inherit"
-                        aria-label="menu"
-                        sx={{ mr: 2 }}
-                    >
-                        <ShoppingCartIcon />
-                    </IconButton>
-                </Toolbar>
-            </AppBar>
+            <Header />
         </>
     );
 }
---
+++ Homework 2/e-commerce/src/Header/Header.tsx
@@ -0,0 +1,32 @@
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+
+export default function Header() {
+    return (
+        <>
+            <AppBar position="static">
+                <Toolbar>
+                    <Typography
+                        variant="h6"
+                        component="div"
+                        sx={{ flexGrow: 1 }}
+                    >
+                        E-Commerce
+                    </Typography>
+                    <IconButton
+                        size="large"
+                        edge="start"
+                        color="inherit"
+                        aria-label="menu"
+                        sx={{ mr: 2 }}
+                    >
+                        <ShoppingCartIcon />
+                    </IconButton>
+                </Toolbar>
+            </AppBar>
+        </>
+    );
+}
