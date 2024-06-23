cf9cc5440e80f9f6a74ecf35361926ccd32223a4
Brayan Durán Medina <brayanduranmedina@gmail.com>
2024-06-23 15:55:05 +0200

    chore: Update project configuration and add initial code files

--- Homework 2/e-commerce/src/App.tsx
+++ Homework 2/e-commerce/src/App.tsx
@@ -1,7 +1,35 @@
 import "./App.css";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 
 function App() {
-    return <>Hello World</>;
+    return (
+        <>
+            <AppBar position="static">
+                <Toolbar>
+                    <Typography
+                        variant="h6"
+                        component="div"
+                        sx={{ flexGrow: 1 }}
+                    >
+                        E-Commerce
+                    </Typography>
+                    <IconButton
+                        size="large"
+                        edge="start"
+                        color="inherit"
+                        aria-label="menu"
+                        sx={{ mr: 2 }}
+                    >
+                        <ShoppingCartIcon />
+                    </IconButton>
+                </Toolbar>
+            </AppBar>
+        </>
+    );
 }
 
 export default App;
